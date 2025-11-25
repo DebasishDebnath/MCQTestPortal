8cbd91151da317ba917c5c71270c4e7d6b3cb0d4
Debasish Debnath <ddebnath241@gmail.com>
2025-11-25 16:27:14 +0530

    repo initialize

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AppRouter from "../src/AppRouters.jsx";
+function App() {
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/AppRouters.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppRouters() {
+  return <div>AppRouters</div>;
+}
+
+export default AppRouters;
